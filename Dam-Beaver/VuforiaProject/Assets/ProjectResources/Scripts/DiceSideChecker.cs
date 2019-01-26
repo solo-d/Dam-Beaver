69fd6f985fdc24e1cceccf34b235a69374e7d212
WinterPu <striveant@outlook.com>
2019-01-26 04:23:06 -0500

    Initial Project
    + Vuforia Test
    + JoyStick Test
    + Dice Test

---
+++ Dam-Beaver/VuforiaProject/Assets/ProjectResources/Scripts/DiceSideChecker.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceSideChecker : MonoBehaviour
+{
+    Vector3 dice_velocity;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        dice_velocity = DiceScript.dice_velocity;
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        Debug.Log("OK");
+        if (dice_velocity.x == 0f && dice_velocity.y == 0f && dice_velocity.z == 0f)
+        {
+            Debug.Log(col.gameObject.name);
+            switch (col.gameObject.name)
+            {
+                // need to assign the opposite side number
+                case "Side1":
+                    TextDiceNumberShow.dice_number = 6;
+                    break;
+                case "Side2":
+                    TextDiceNumberShow.dice_number = 5;
+                    break;
+                case "Side3":
+                    TextDiceNumberShow.dice_number = 4;
+                    break;
+                case "Side4":
+                    TextDiceNumberShow.dice_number = 3;
+                    break;
+                case "Side5":
+                    TextDiceNumberShow.dice_number = 2;
+                    break;
+                case "Side6":
+                    TextDiceNumberShow.dice_number = 1;
+                    break;
+            }
+        }
+    }
+}
