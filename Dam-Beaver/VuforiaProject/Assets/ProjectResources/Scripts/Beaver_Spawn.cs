480f05af6e841647441f3dcd00675431e6181f4c
solomon <kingds41@gmail.com>
2019-01-26 21:17:10 -0500

    Added script

---
+++ Dam-Beaver/VuforiaProject/Assets/ProjectResources/Scripts/Beaver_Spawn.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Beaver_Spawn : MonoBehaviour
+{
+    public GameObject beaver_Prefab_gameObject;
+
+    private void Start()
+    {
+        Invoke("spawn", 2f);
+    }
+
+    void spawn() {
+        Instantiate(beaver_Prefab_gameObject, transform.position, Quaternion.identity);
+    }
+}
